5284a32006d01f4e77c8328ef3284ef317493431
Conripi <mlksanno@gmail.com>
2021-09-21 22:41:16 +0900

    fullcalendarスマホ対応

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 $(function(){
-    $("#today > .btn-group").append("<p>aaaaa</p>");
 
-    
+
+
     $.getJSON("https://conripi.github.io/arknights_birthday/data/data.json", (data) => {
+        $("#today > .btn-group").append("<p>aaaaa</p>");
+        
+        /*
         let birthday_data = [];
         let year = new Date().getFullYear();
 
@@ -15,6 +18,42 @@ $(function(){
             birthday_data.push(json);
         }
 
+        let birthday_json = JSON.stringify(birthday_data)
+
+        var calendarEl = document.getElementById("calendar");
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            dayCellContent: function(e) {
+                e.dayNumberText = e.dayNumberText.replace('日', '');
+            },
+            locale: 'ja',
+            height: 'auto',
+            firstDay: 1,
+            expandRows: true, // 画面に合わせて高さを再設定
+            selectable: true,
+            headerToolbar: {
+                left: "",
+                center: "title",
+                right: "today prev,next"
+            },
+            buttonText: {
+                today: '今月',
+                month: '月',
+                list: 'リスト'
+            },
+            events: JSON.parse(birthday_json),
+            eventClick: function(info) {
+                info.jsEvent.preventDefault(); // don't let the browser navigate
+
+                if (info.event.url) {
+                    window.open(info.event.url);
+                }
+            },
+        });
+
+        $("calendar").addTouch();
+
+        calendar.render();
+
         //今日の誕生日
         for(let i = 0; i < birthday_data.length; i++){
             if(birthday_data[i].start !== moment().format("YYYY-MM-DD")) continue;
@@ -22,6 +61,7 @@ $(function(){
             let url = birthday_data[i].url;
             $("#today > .btn-group").append("<div><button style='margin-left: 20px;' type=\"button\" class=\"btn btn-lg btn-primary\" onclick=\"window.open('"+url+"', '_blank')\">"+birthday_data[i].title+"</button></div>")
         }
+        */
     })
 })
 
